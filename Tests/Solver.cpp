eec19e02510e5730d54b95b8d132aad4a4d302ac
Hiromasa Kato <hiromasa@iwate-u.ac.jp>
2016-03-24 14:00:18 +0900

    - removed clutter

--- Tests/Solver.cpp
+++ Tests/Solver.cpp
@@ -17,7 +17,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-// $Id: Solver.cpp 313 2013-12-13 08:50:32Z kato $
 
 #include "Communicator.h"
 #include "Profiler.h"
